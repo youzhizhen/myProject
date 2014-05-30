1c756e61d10f09257e4460516caec08430fcf08d
LiYing <ying.li@servision.com.cn>
2014-05-30 10:21:29 +0800

    Signed-off-by: LiYing <ying.li@servision.com.cn>

--- src/main/java/com/liying/Test.java
+++ src/main/java/com/liying/Test.java
@@ -7,7 +7,6 @@ public class Test extends BeanFactoryUtils{
     public static void main(String[] args) {
         
         System.out.println("test this  branch ");
-        System.out.println("test this liying branch");
-        System.out.println("test the other branch");
+
     }
 }
