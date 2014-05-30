33b2b72a9aadf56b4a83c79c5d186c6205ed3ea3
LiYing <ying.li@servision.com.cn>
2014-05-30 09:48:59 +0800

    Signed-off-by: LiYing <ying.li@servision.com.cn>

--- src/main/java/com/liying/Test.java
+++ src/main/java/com/liying/Test.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.liying.service.MethodInject;
-
 public class Test extends BeanFactoryUtils{
 
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
   
-
+        System.out.println("test the system ");
     }
 }
